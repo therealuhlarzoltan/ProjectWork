ad35356c29b691f1c77310e412e2e7d7d727f2ab
therealuhlarzoltan <therealuhlarzoltan@gmail.com>
2024-12-15 22:58:31 +0100

    Separated train head and tail movements in TrainLeavesTrackEvent

--- src/main/java/hu/uni/obuda/des/railways/events/movement/TrainLeavesTrackEvent.java
+++ src/main/java/hu/uni/obuda/des/railways/events/movement/TrainLeavesTrackEvent.java
@@ -1,6 +1,8 @@
 package hu.uni.obuda.des.railways.events.movement;
 
 import hu.uni.obuda.des.core.simulation.AbstractSimulator;
+import hu.uni.obuda.des.railways.events.signalling.SignallingEvent;
+import hu.uni.obuda.des.railways.installations.SignallingSystem;
 import hu.uni.obuda.des.railways.tracks.Track;
 import hu.uni.obuda.des.railways.trains.Train;
 import lombok.Getter;
@@ -16,6 +18,25 @@ public class TrainLeavesTrackEvent extends TrainMovementEvent {
 
     @Override
     public void execute(AbstractSimulator simulator) {
+        //Logs current event
         System.out.println("Train " + train.toString() + " leaves the track " + track.getId() + " at time " + getEventTime());
+        //Checks if signalling required
+        if (track instanceof SignallingSystem sys) {
+            var signallingEvent = createSignallingEvent(this, train);
+            simulator.insert(signallingEvent);
+        }
+        //Releases the track
+        track.release();
     }
+
+    /**
+     * Creates a SignallingEvent to notify the signalling system that the tail of the train triggered the sensors.
+     * @param movementEvent The event that triggered the signalling event.
+     * @param train The train whose tail triggered the signalling system's sensors.
+     * @return  A new SignallingEvent that is scheduled to occur immediately at the current time.
+     */
+    private SignallingEvent createSignallingEvent(TrainMovementEvent movementEvent, Train train) {
+        return new SignallingEvent(movementEvent, train, train.getCurrentDirection());
+    }
+
 }
