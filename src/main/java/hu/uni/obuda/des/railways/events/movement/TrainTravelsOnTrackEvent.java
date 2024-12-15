141d52983f310c3c0956cdd3dc1403eb3b6f4458
therealuhlarzoltan <therealuhlarzoltan@gmail.com>
2024-12-15 01:16:17 +0100

    Moved the track property to the TrainMovementEvent

--- src/main/java/hu/uni/obuda/des/railways/events/movement/TrainTravelsOnTrackEvent.java
+++ src/main/java/hu/uni/obuda/des/railways/events/movement/TrainTravelsOnTrackEvent.java
@@ -9,52 +9,50 @@ import hu.uni.obuda.des.railways.tracks.Track;
 import hu.uni.obuda.des.railways.trains.Train;
 
 public class TrainTravelsOnTrackEvent extends TrainMovementEvent {
-    private final Track currentTrack;
     private final Track previousTrack;
 
     public TrainTravelsOnTrackEvent(double eventTime, Train train, Track currentTrack, Track previousTrack) {
-        super(eventTime, train);
-        this.currentTrack = currentTrack;
+        super(eventTime, train, currentTrack);
         this.previousTrack = previousTrack;
     }
 
     @Override
     public void execute(AbstractSimulator simulator) {
-        System.out.println("Train " + train.toString() + " is travelling on track " + currentTrack.toString() + " at time " + getEventTime());
-        if (currentTrack instanceof SignallingSystem) {
+        System.out.println("Train " + train.toString() + " is travelling on track " + track.toString() + " at time " + getEventTime());
+        if (track instanceof SignallingSystem) {
             simulator.insert(new SignallingEvent(this, train, train.getCurrentDirection()));
             previousTrack.release();
-            double travelTime = (currentTrack.getLengthInKm() / Math.max(train.getMaxSpeed(), currentTrack.getMaxSpeed())) * 60;
+            double travelTime = (track.getLengthInKm() / Math.max(train.getMaxSpeed(), track.getMaxSpeed())) * 60;
             Track nextTrack = train.getRoute().poll();
             if (nextTrack instanceof Station.Platform && train.getStops().get(train.getStops().indexOf(train.getCurrentStation()) + 1).equals(((Station.Platform) nextTrack).getStation())) {
                 simulator.insert(new TrainArrivalEvent(getEventTime() + travelTime, train, (Station.Platform) nextTrack));
             } else {
-                simulator.insert(new TrainTravelsOnTrackEvent(getEventTime() + travelTime, train, nextTrack, currentTrack));
+                simulator.insert(new TrainTravelsOnTrackEvent(getEventTime() + travelTime, train, nextTrack, track));
             }
-        } else if (currentTrack instanceof DirectionalResource dirTrack) {
+        } else if (track instanceof DirectionalResource dirTrack) {
             if (!dirTrack.occupy(train, train.getCurrentDirection())) {
-                simulator.insert(new TrainStopsEvent(getEventTime(), train, currentTrack, previousTrack));
+                simulator.insert(new TrainStopsEvent(getEventTime(), train, track, previousTrack));
             } else {
                 previousTrack.release();
-                double travelTime = (currentTrack.getLengthInKm() / Math.max(train.getMaxSpeed(), currentTrack.getMaxSpeed())) * 60;
+                double travelTime = (track.getLengthInKm() / Math.max(train.getMaxSpeed(), track.getMaxSpeed())) * 60;
                 Track nextTrack = train.getRoute().poll();
                 if (nextTrack instanceof Station.Platform && train.getStops().get(train.getStops().indexOf(train.getCurrentStation()) + 1).equals(((Station.Platform) nextTrack).getStation())) {
                     simulator.insert(new TrainArrivalEvent(getEventTime() + travelTime, train, (Station.Platform) nextTrack));
                 } else {
-                    simulator.insert(new TrainTravelsOnTrackEvent(getEventTime() + travelTime, train, nextTrack, currentTrack));
+                    simulator.insert(new TrainTravelsOnTrackEvent(getEventTime() + travelTime, train, nextTrack, track));
                 }
             }
         }
-        else if (!currentTrack.occupy(train)) {
-            simulator.insert(new TrainStopsEvent(getEventTime(), train, currentTrack, previousTrack));
+        else if (!track.occupy(train)) {
+            simulator.insert(new TrainStopsEvent(getEventTime(), train, track, previousTrack));
         } else {
             previousTrack.release();
-            double travelTime = (currentTrack.getLengthInKm() / Math.max(train.getMaxSpeed(), currentTrack.getMaxSpeed())) * 60;
+            double travelTime = (track.getLengthInKm() / Math.max(train.getMaxSpeed(), track.getMaxSpeed())) * 60;
             Track nextTrack = train.getRoute().poll();
             if (nextTrack instanceof Station.Platform && train.getStops().get(train.getStops().indexOf(train.getCurrentStation()) + 1).equals(((Station.Platform) nextTrack).getStation())) {
                 simulator.insert(new TrainArrivalEvent(getEventTime() + travelTime, train, (Station.Platform) nextTrack));
             } else {
-                simulator.insert(new TrainTravelsOnTrackEvent(getEventTime() + travelTime, train, nextTrack, currentTrack));
+                simulator.insert(new TrainTravelsOnTrackEvent(getEventTime() + travelTime, train, nextTrack, track));
             }
         }
     }
